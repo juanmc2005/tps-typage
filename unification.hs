6ff396436d98682b8438e1bc8616eae89020d781
Juanma Coria <juanmc2005@hotmail.com>
2019-02-10 18:07:37 +0100

    First working solution where the unify function says if a system of equations is solvable or not

--- unification.hs
+++ unification.hs
@@ -6,8 +6,6 @@
 
 import Data.List
 import Data.Maybe
-import Data.Set (Set)
-import qualified Data.Set as Set
 
 -- Definition of a type
 data Type = TInt | TBool | TVar Int | TPair Type Type | TFunction Type Type deriving (Eq, Ord)
@@ -22,43 +20,65 @@ instance Show Type where
 type Equation = (Type, Type)
 
 -- Definition of an equation system
-type EquationSystem = Set Equation
+type EquationSystem = [Equation]
+
+-- Definition of a mapping
+type Mapping = (Int, Type)
 
 -- Definition of a substitution: a set of variable to type pairs,
 -- where the variable is represented as an Int (as in TVar Int)
-type Substitution = [(Int, Type)]
+type Substitution = [Mapping]
 
 
 -- Unification algorithm
--- unify :: EquationSystem -> Maybe Substitution
+unify :: EquationSystem -> Bool
+unify eqs = if isSolved eqs
+              then True
+              else let newEqs = eqSysUnificationStep eqs in
+                     if isJust newEqs then unify $ fromJust newEqs else False
+
+-- Perform a single step of unification with the first equation that allows for it in an equation system
+eqSysUnificationStep :: EquationSystem -> Maybe EquationSystem
+eqSysUnificationStep eqs = fromMaybe Nothing newEqs
+                             where
+                                unificationStep = \eq -> eqUnificationStep (delete eq eqs) eq
+                                newEqs = find isJust $ map unificationStep eqs
 
 -- Perform a step of the unification algorithm for an equation
-unificationStep :: EquationSystem -> Equation -> EquationSystem
--- Exchange rule
-unificationStep eqs (t, TVar v)                         = Set.insert (TVar v, t) eqs
--- Decomposition rule for pairs
-unificationStep eqs (TPair s1 s2, TPair t1 t2)          = Set.union eqs $ Set.fromList [(s1, t1), (s2, t2)]
--- Decomposition rule for functions
-unificationStep eqs (TFunction s1 s2, TFunction t1 t2)  = Set.union eqs $ Set.fromList [(s1, t1), (s2, t2)]
--- Replacement rule
-unificationStep eqs (TVar v, s)                         = if not (hasVar v s)
-                                                          then Set.insert (TVar v, s) $ substituteEqSystem [(v, s)] eqs
-                                                          else Set.insert (TVar v, s) eqs
--- Elimination rule
-unificationStep eqs (t1, t2)                            = if t1 == t2 then eqs else Set.insert (t1, t2) eqs
+eqUnificationStep :: EquationSystem -> Equation -> Maybe EquationSystem
+eqUnificationStep eqs (t1, t2) = if t1 == t2 then Just eqs else applyUnificationRule eqs (t1, t2) -- Elimination
+
+-- Apply a unification rule other than elimination
+applyUnificationRule :: EquationSystem -> Equation -> Maybe EquationSystem
+applyUnificationRule eqs (TVar v, s)                        = if eqSysHasVar v eqs && not (hasVar v s) -- Replacement
+                                                                then Just $ (TVar v, s):(substituteEqSystem [(v, s)] eqs)
+                                                                else Nothing
+applyUnificationRule eqs (t, TVar v)                        = Just $ (TVar v, t):eqs             -- Exchange
+applyUnificationRule eqs (TPair s1 s2, TPair t1 t2)         = Just $ [(s1, t1), (s2, t2)] ++ eqs -- Decomposition
+applyUnificationRule eqs (TFunction s1 s2, TFunction t1 t2) = Just $ [(s1, t1), (s2, t2)] ++ eqs -- Decomposition
+applyUnificationRule _   _                                  = Nothing
 
 -- Checks if an equation system is in solved form
 isSolved :: EquationSystem -> Bool
-isSolved eqs = Set.foldr (&&) True (Set.map isSolvedForm eqs)
+isSolved eqs = foldr (&&) True (map isSolvedForm eqs)
 
 -- Checks if an equation is in solved form
 isSolvedForm :: Equation -> Bool
 isSolvedForm (TVar _, term) = vars term == 0
 isSolvedForm _              = False
 
+-- Transforms an equation system into a substitution
+toSubstitution :: EquationSystem -> Maybe Substitution
+toSubstitution eqs = if isSolved eqs then Just $ map (fromJust . toMapping) eqs else Nothing
+
+-- Transforms an equation into a substitution
+toMapping :: Equation -> Maybe Mapping
+toMapping (TVar v, term) = Just (v, term)
+toMapping _              = Nothing
+
 -- Applies a substitution to an equation system
 substituteEqSystem :: Substitution -> EquationSystem -> EquationSystem
-substituteEqSystem sub eqs = Set.map (substituteEquation sub) eqs
+substituteEqSystem sub eqs = map (substituteEquation sub) eqs
 
 -- Applies a substitution to an equation
 substituteEquation :: Substitution -> Equation -> Equation
@@ -75,6 +95,14 @@ substituteTerm sub (TFunction t1 t2) = TFunction (substituteTerm sub t1) (substi
 -- Find the term (Type) that substitutes a given variable (int) in a substitution
 findTerm :: Substitution -> Int -> Maybe Type
 findTerm sub v = fmap snd $ find (\x -> (fst x) == v) sub
+
+-- Checks if a variable is present in an equation system
+eqSysHasVar :: Int -> EquationSystem -> Bool
+eqSysHasVar v eqs = foldr (||) False $ map (eqHasVar v) eqs
+
+-- Checks if a variable is present in an equation
+eqHasVar :: Int -> Equation -> Bool
+eqHasVar v (t1, t2) = hasVar v t1 || hasVar v t2
 
 -- Checks if a variable (Int as in TVar Int) appears in a term (Type)
 hasVar :: Int -> Type -> Bool
@@ -95,4 +123,6 @@ vars (TFunction t1 t2) = vars t1 + vars t2
 
 ---------------------------------------------------------------------------------------------------
 
-main = print $ isSolved $ Set.fromList [(TVar 10, TFunction TInt TBool), ((TVar 2), TPair TInt TInt)]
+main = print $ unify eqs
+         where 
+            eqs = [(TVar 0, TVar 1), (TVar 2, TVar 3), (TVar 3, TInt), (TVar 4, TFunction (TInt) (TVar 1)), (TVar 1, TInt)]
